53f392764343e58c736f405a521536e6ec8c5abe
guillaumedauplat <guillaume.dauplat@gmail.com>
2018-05-08 17:41:27 +0200

    missing update def

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+tasks = Task.create([{ title: 'Star Wars', details:'Best movies ever'}, { title: 'deadpool', details:'funniest Character'}])
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
